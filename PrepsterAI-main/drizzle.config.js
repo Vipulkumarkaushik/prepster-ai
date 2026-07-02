37282f9388302411f4256db42c46d19e9fd65c75
Vipulkumarkaushik <vipul.kumar.ug23@nsut.ac.in>
2026-07-02 16:31:28 +0530

    Hardcoded config for deployment

--- PrepsterAI-main/drizzle.config.js
+++ PrepsterAI-main/drizzle.config.js
@@ -1,13 +1,8 @@
 /** @type { import("drizzle-kit").Config } */
-import * as dotenv from 'dotenv';
-// Vercel aur local dono ke liye sahi path load karna
-dotenv.config({ path: '.env' }); 
-
 export default {
     schema: "./utils/schema.js",
     dialect: 'postgresql',
     dbCredentials: {
-      // Yahan ab hum process.env ka use karenge
-      url: process.env.DATABASE_URL, 
+      url: 'postgresql://neondb_owner:npg_9bTDziBpKL3H@ep-odd-scene-aighsdor-pooler.c-4.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require',
     },
 };
